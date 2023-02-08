752de820cdaaccd53391b346dd000527c3ae0323
Felipe B. de Moura <fittato@gmail.com>
2023-02-08 11:28:32 -0300

    Finalizada a aula 05 do curso Python avançando na linguagem

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,20 +4,34 @@ def jogar():
     print('*********************************')
 
     palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    letras_acertadas = ["_" for _ in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while not enforcou and not acertou:
         chute = input("Qual letra?").upper().strip()
-        index = 0
-        for letra in palavra_secreta:
-            if chute == letra:
-                letras_acertadas[index] = letra
-            index = index + 1
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, você errou! Faltam {6 - erros} tentativas.")
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+        if acertou:
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
         print(letras_acertadas)
 
     print('Fim do jogo')
