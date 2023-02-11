0614fa2aed3bb7b2bc44ae01eff409e7c9a9b064
Felipe B. de Moura <fittato@gmail.com>
2023-02-11 16:27:42 -0300

    Finalizada a aula 03 do curso Python Orientação a Objetos

--- oo/conta.py
+++ oo/conta.py
@@ -5,3 +5,15 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(f"Saldo de {self.saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+        else:
+            print(f"Saldo insuficiente para sacar {valor} do titular {self.titular}")
