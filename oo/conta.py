258951f59a63e72fc36f77b99315f6f6bcd72f63
Felipe B. de Moura <fittato@gmail.com>
2023-02-13 14:58:19 -0300

    Finalizada a aula 04 do curso Python Orientação a Objetos

--- oo/conta.py
+++ oo/conta.py
@@ -1,19 +1,23 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo o objeto... {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo de {self.saldo} do titular {self.titular}")
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        if self.saldo >= valor:
-            self.saldo -= valor
+        if self.__saldo >= valor:
+            self.__saldo -= valor
         else:
-            print(f"Saldo insuficiente para sacar {valor} do titular {self.titular}")
+            print(f"Saldo insuficiente para sacar {valor} do titular {self.__titular}")
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
