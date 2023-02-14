d057fd73d23dbfedb3994422befc3c1bd91df3c8
Felipe B. de Moura <fittato@gmail.com>
2023-02-14 18:14:01 -0300

    Finalizada a aula 06 do curso Python entendendo Orientaçãoa Objetos

--- oo/conta.py
+++ oo/conta.py
@@ -5,6 +5,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "001"
 
     def extrato(self):
         print(f"Saldo de {self.__saldo} do titular {self.__titular}")
@@ -12,11 +13,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        if self.__saldo >= valor:
+        if self.__pode_sacar(valor):
             self.__saldo -= valor
         else:
-            print(f"Saldo insuficiente para sacar {valor} do titular {self.__titular}")
+            print(f"O valor {valor} passou o limite.")
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -40,3 +45,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_banco():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
