e128e4364c37da5f2546bd3d1c1f5f439833f92f
Felipe B. de Moura <fittato@gmail.com>
2023-02-22 12:10:06 -0300

    Finalizada a aula 04 do curso Python avançando na orientação a objetos

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -44,22 +44,47 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+# print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao}: {vingadores.likes}')
 
 vingadores.dar_like()
-
-print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao}: {vingadores.likes}')
-
-atlanta = Serie('atlanta', 2018, 2)
-
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 
-atlanta.dar_like()
+# print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas}: {atlanta.likes}')
 
-print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas}: {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 
-filmes_series = [vingadores, atlanta]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_series:
-    # detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
