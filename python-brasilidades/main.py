87bb4aeb78b8dffdd9cec40af7b7e9ddcc47d020
FelipeBMoura <fittato@gmail.com>
2023-03-12 19:28:51 -0300

    Finalizada a aula 02 do curso Python Brasil validação de dados no padrão nacional

--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,5 +1,15 @@
-from cpf import Cpf
+from cpf_cnpj import Documento
+from validate_docbr import CNPJ
 
-cpf_um = Cpf("97686085015")
 
-print(cpf_um)
+# cpf_um = Cpf("97686085015")#
+# print(cpf_um)
+
+exemplo_cnpj = "35379838000112"
+exemplo_cpf = "32007832062"
+
+# cnpj = CNPJ()#
+# print(cnpj.validate(exemplo_cnpj))
+
+documento = Documento.cria_documento(exemplo_cpf)
+print(documento)
