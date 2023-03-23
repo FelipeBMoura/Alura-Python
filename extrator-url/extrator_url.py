f9e9a78613cb1a270b14b6562578c617052591f0
Felipe B. de Moura <fittato@gmail.com>
2023-03-22 21:31:56 -0300

    Finalizada a aula 05 do curso String em Python extraindo informações de uma URL

--- extrator-url/extrator_url.py
+++ extrator-url/extrator_url.py
@@ -43,8 +43,18 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other):
+        return self.url == other.url
 
 url = 'https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real'
 extrator_url = ExtratorURL(url)
+print("O tamanho da URl: ", len(extrator_url))
+print(extrator_url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
