6fcb52b37ee8bffde085df66e1aec4ebd51cd876
Felipe B. de Moura <fittato@gmail.com>
2023-03-30 13:06:40 -0300

    Finalizada a aula 4 do curso Python Collections parte 1: listas e tuplas

--- collections/introcucao_a_collections.py
+++ collections/introcucao_a_collections.py
@@ -66,3 +66,45 @@ print(numeros)
 numeros = numeros + 3
 
 print(numeros)
+
+
+class ContaSalario:
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def __eq__(self, outro):
+        if type(outro) != ContaSalario:
+            return False
+        return self._codigo == outro._codigo and self._saldo == outro._saldo
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return f'[>>Código {self._codigo} Saldo {self._saldo}<<]'
+
+
+class ContaMultiploSalario(ContaSalario):
+    pass
+
+
+conta1 = ContaSalario(37)
+print(conta1)
+
+conta2 = ContaCorrente(37)
+print(conta2)
+
+print(conta1 == conta2)
+
+contas = [conta1]
+
+print(conta1 in contas)
+
+print(conta1 in [conta2])
+
+print(conta2 in [conta1])
+
+print(conta2 in contas)
+
+
