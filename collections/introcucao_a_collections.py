7dd0d0ceb93b0501be51da9278cd7cb95863d483
Felipe B. de Moura <fittato@gmail.com>
2023-03-31 13:13:02 -0300

    Finalizada a aula 5 do curso Python Collections parte 1: listas e tuplas

--- collections/introcucao_a_collections.py
+++ collections/introcucao_a_collections.py
@@ -107,4 +107,23 @@ print(conta2 in [conta1])
 
 print(conta2 in contas)
 
+idades = [15, 87, 65, 56, 32, 49, 37]
 
+for i in range(len(idades)):
+    print(i, idades[i])
+
+print(list(range(len(idades))))
+
+print(list(enumerate(idades)))
+
+for indice, idade in enumerate(idades):
+    print(indice, 'x' , idade)
+
+usuarios = [
+    ("Guilherme", 37, 1981),
+    ("Daniela", 31, 1987),
+    ("Paulo", 39, 1979),
+]
+
+for nome, idade, nascimento in usuarios:
+    print(nome)
