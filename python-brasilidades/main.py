4893bcfcaf6ade5a4d801c5605839bc895709458
Felipe B. de Moura <fittato@gmail.com>
2023-04-18 13:17:40 -0300

    Finalizada a aula 03 do curso Python Brasil: validação de dados no padrão nacional

--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,15 +1,41 @@
-from cpf_cnpj import Documento
-from validate_docbr import CNPJ
+import re
 
+from telefonesBr import TelefonesBr
 
-# cpf_um = Cpf("97686085015")#
-# print(cpf_um)
+padrao = "[0-9][a-z][0-9]"  # padrão número letra número
+texto = "123 1a2 1cc aa1"
+resposta = re.search(padrao, texto)
 
-exemplo_cnpj = "35379838000112"
-exemplo_cpf = "32007832062"
+print(resposta)
+print(resposta.group())
 
-# cnpj = CNPJ()#
-# print(cnpj.validate(exemplo_cnpj))
+padra_email = "\w{5,50}@\w[a-z]{3,10}.\w{2,3}.\w{2,3}"
 
-documento = Documento.cria_documento(exemplo_cnpj)
-print(documento)
+texto = "aaabbbccc rodrigo123@gmail.com.br ihgsufyufyf"
+
+resposta = re.search(padra_email, texto)
+
+print(resposta)
+print(resposta.group())
+
+padrao_molde = "(xx)aaaa-wwww"
+
+padrao_telefone = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+
+texto = "eu gosto do numero 2126451234 e gosto também do 2136431234"
+
+resposta = re.search(padrao_telefone, texto)
+
+print(resposta)
+print(resposta.group())
+
+telefone = "552126481234"
+
+telefone_objeto = TelefonesBr(telefone)
+
+padrao = "([0-9]{2,3})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
+resposta = re.search(padrao, telefone)
+print(resposta)
+print(resposta.group(2))
+
+print(telefone_objeto)
