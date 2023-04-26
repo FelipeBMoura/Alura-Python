93c484169f7024f57f938c3c1af1abc13bdbb55f
Felipe B. de Moura <fittato@gmail.com>
2023-04-26 13:07:33 -0300

    Finalizada a aula 02 do curso Python e TDD

---
+++ python_e_tdd/tests/test_bytebank.py
@@ -0,0 +1,18 @@
+from python_e_tdd.bytebank import Funcionario
+
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = '13/03/2000'  # Given-Contexto
+        esperado = 23
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade()  # When-ação
+        assert resultado == esperado  # then-desfecho
+
+    def test_quando_sobrenome_recebe_lucas_carvalho_deve_retornar_carvalho(self):
+        entrada = ' Lucas Carvalho '  # Given
+        esperado = 'Carvalho'
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome()  # when
+        assert resultado == esperado
+
